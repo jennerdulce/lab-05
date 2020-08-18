5dfe47673713293da0eeec5fdec2cf8b842eb9c0
Jenner Dulce <jennerdulce@gmail.com>
2020-08-17 19:30:08 -0700

    encapsulated previous written code

--- lab/practice/add-content.js
+++ lab/practice/add-content.js
@@ -1,31 +1,41 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
 
-if (hourNow > 18) {
-    greeting = 'Good evening!';
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-} else if (hourNow > 0) {
-    greeting = 'Good morning!';
-} else {
-    greeting = 'Welcome';
+function getHour() {
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+
+    if (hourNow > 18) {
+        greeting = 'Good evening!';
+    } else if (hourNow > 12) {
+        greeting = 'Good afternoon!';
+    } else if (hourNow > 0) {
+        greeting = 'Good morning!';
+    } else {
+        greeting = 'Welcome';
+    }
+
+    var el = document.getElementById('message');
+    el.textContent = greeting;
 }
 
-var el = document.getElementById('message');
-el.textContent = greeting;
+function changeBackground(question) {
+    var userinput = prompt(question)
+    var backgroundColor = document.getElementById('background');
+    backgroundColor.textContent = "body {background: " + userinput + "}"
+}
 
+getHour()
+changeBackground("Type a color to change the color of the background")
 alert("For the king in the North!!!!") 
-var enter = confirm("Press okay to proceed.")
+
+
+
+
+// var enter = confirm("Press okay to proceed.")
 // if (enter == true ) {
 //     enter;
 // } else {
 //     close window
 // }
 // console.log("Enter value", enter)
-
-var userinput = prompt("Type a color to change the color of the background")
-
-var backgroundColor = document.getElementById('background');
-backgroundColor.textContent = "body {background: " + userinput + "}"
 
