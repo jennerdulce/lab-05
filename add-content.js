8af541737ad69a5f71215c9f0efe1ca8b1477090
Jenner Dulce <61197981+jennerdulce@users.noreply.github.com>
2020-08-18 16:27:46 -0700

    Add files via upload
    added files

---
+++ add-content.js
@@ -0,0 +1,31 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome';
+}
+
+var el = document.getElementById('message');
+el.textContent = greeting;
+
+alert("For the king in the North!!!!") 
+var enter = confirm("Press okay to proceed.")
+// if (enter == true ) {
+//     enter;
+// } else {
+//     close window
+// }
+// console.log("Enter value", enter)
+
+var userinput = prompt("Type a color to change the color of the background")
+
+var backgroundColor = document.getElementById('background');
+backgroundColor.textContent = "body {background: " + userinput + "}"
+
