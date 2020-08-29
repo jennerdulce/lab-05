804eaebd481f4f5472a0f21f2496327bdaeabf03
Jenner Dulce <jennerdulce@gmail.com>
2020-08-28 20:44:40 -0700

    added buttons to change background color

--- add-content.js
+++ add-content.js
@@ -18,12 +18,14 @@ function greetingMessage() {
     el.textContent = greeting;
 }
 
-function changeBackground() {
-    var userinput = prompt("Type a color to change the color of the background")
+// function changeBackground() {
+//     var userinput = prompt("Type a color to change the color of the background")
 
-    var backgroundColor = document.getElementById('background');
-    backgroundColor.textContent = "body {background: " + userinput + "}"
-}
+//     var backgroundColor = document.getElementById('background');
+//     backgroundColor.textContent = "body {background: " + userinput + "}"
+// }
+
+
 
 function nameMessage() {
     var name = prompt('What is your name?');
@@ -32,49 +34,92 @@ function nameMessage() {
 }
 
 
-function getPreference() {
-    var response = prompt('Would you like an Apple or a Cookie?');
-    var item;
+// function getPreference() {
+//     var response = prompt('Would you like an Apple or a Cookie?');
+//     var item;
     
-    while(response != 'cookie' && response != 'apple') {
-        response = prompt('Choose Apple or Cookie');
-    }
+//     while(response != 'cookie' && response != 'apple') {
+//         response = prompt('Choose Apple or Cookie');
+//     }
 
-    if(response == 'apple') {
-        item = ' <img width="80px" height="80px" src="apple.jpeg" />';
-    } else if (response == 'cookie') {
-        item = ' <img width="80px" height="80px" src="cookie.png" />';
-    }
+//     if(response == 'apple') {
+//         item = ' <img width="80px" height="80px" src="apple.jpeg" />';
+//     } else if (response == 'cookie') {
+//         item = ' <img width="80px" height="80px" src="cookie.png" />';
+//     }
 
-    return item;
+//     return item;
     
-}   
+// }   
 
-function getFood() {
-    var counting = prompt('How many would you like?');
-    while(isNaN(counting) || counting ==='') {
-        var counting = prompt('Please type a number for how many  you would like.');
-    }
-    return counting;
-}
+// function getFood() {
+//     var counting = prompt('How many would you like?');
+//     while(isNaN(counting) || counting ==='') {
+//         var counting = prompt('Please type a number for how many  you would like.');
+//     }
+//     return counting;
+// }
 
-function loopingFood(x, food) {
-    var msg = '';
-    for(i = 0; i < x; i++) {
-        document.write(food);
-    }
-}
+// function loopingFood(x, food) {
+//     var msg = '';
+//     for(i = 0; i < x; i++) {
+//         document.write(food);
+//     }
+// }
 
-function displayFood() {
-    var preference = getPreference()
-    var numOfFood = getFood();
-    loopingFood(numOfFood, preference);
-}
+// function displayFood() {
+//     var preference = getPreference()
+//     var numOfFood = getFood();
+//     loopingFood(numOfFood, preference);
+// }
 
 
 
 greetingMessage();
-changeBackground();
+// changeBackground();
 nameMessage(); 
-displayFood();
+// displayFood();
 // // alert("For the king in the North!!!!") ;
+
+
+// change background colors
+
+function colorRed() {
+    document.getElementById('background').innerHTML = 
+    "body {background: tomato}"
+}
+
+function colorBlue() {
+    document.getElementById('background').innerHTML = 
+    "body {background: DodgerBlue}"
+}
+
+function colorGreen() {
+    document.getElementById('background').innerHTML = 
+    "body {background: MediumSeaGreen}"
+}
+
+function colorOrange() {
+    document.getElementById('background').innerHTML = 
+    "body {background: Orange}"
+}
+
+function colorViolet() {
+    document.getElementById('background').innerHTML = 
+    "body {background: Violet}"
+}
+
+function colorHoneydew() {
+    document.getElementById('background').innerHTML = 
+    "body {background: Honeydew}"
+}
+
+function colorHotPink() {
+    document.getElementById('background').innerHTML = 
+    "body {background: HotPink}"
+}
+
+function colorPurple() {
+    document.getElementById('background').innerHTML = 
+    "body {background: MediumPurple}"
+}
